079783ddb198109bd7a84665706f8b8ba3692d0b
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-02-10 20:48:47 -0500

    working todo list

--- components/checklist.jsx
+++ components/checklist.jsx
@@ -11,42 +11,59 @@ import {
 } from 'material-ui/Table';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
 
-// const Item = () => (
+export default class CheckList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { id: 0, name: "foo", done: true },
+        { id: 1, name: "bar", done: false }
+      ]
+    }
+  }
 
-//   <TextField
-//     hintText="Hint Text"
-//   />
-// )
+  toggleTodo(id) {
+    const index = this.state.todos.findIndex((todo) => todo.id == id)
+    const todo = this.state.todos[index]
+    const newTodos = this.state.todos
+    newTodos[index] = Object.assign({}, todo, {done: !todo.done})
+    this.setState({
+      todos: newTodos
+    })
+  }
 
-const todos = [
-  { name: "foo", done: true },
-  { name: "bar", done: false }
-]
-
-export default () => (
-  <Card style={{margin: "24px" }}>
-    <CardTitle title="Todos" subtitle="Todo List" />
-    <CardText>
-      <Table>
-        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-          <TableRow>
-            <TableHeaderColumn>Done</TableHeaderColumn>
-            <TableHeaderColumn>Item</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody displayRowCheckbox={false}>
-          { todos.map(todo =>
-          <TableRow>
-            <TableRowColumn>
-              <Checkbox
-                checked={todo.done}
-              />
-            </TableRowColumn>
-            <TableRowColumn>{todo.name}</TableRowColumn>
-          </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </CardText>
-  </Card>
-);
+  render() {
+    return (
+      <Card style={{margin: "24px" }}>
+        <CardTitle title="Todos" subtitle="Todo List" />
+        <CardText>
+          <Table>
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+              <TableRow>
+                <TableHeaderColumn>Done</TableHeaderColumn>
+                <TableHeaderColumn>Item</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+              { this.state.todos.map(todo =>
+              <TableRow>
+                <TableRowColumn>
+                  <Checkbox
+                    checked={todo.done}
+                    onCheck={this.toggleTodo.bind(this, todo.id)}
+                  />
+                </TableRowColumn>
+                <TableRowColumn>
+                  <div style={todo.done ? {textDecoration: "line-through"} : {} }>
+                    {todo.name}
+                  </div>
+                </TableRowColumn>
+              </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </CardText>
+      </Card>
+    )
+  }
+}
