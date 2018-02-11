94344b4a532080fc59b7d4e716afc0579444f822
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-02-11 00:04:30 -0500

    column drop

---
+++ pages/responsive/columndrop.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <div className="container">
+      <div className="box red"></div>
+      <div className="box green"></div>
+      <div className="box blue"></div>
+    </div>
+    <style jsx global>{`
+      .container {
+        display: flex;
+        flex-wrap: wrap;
+      }
+
+      .box {
+        width: 100%;
+        height: 120px;
+      }
+      .red {
+        background-color: red
+      }
+      .green {
+        background-color: green
+      }
+      .blue {
+        background-color: blue
+      }
+
+      @media screen and (min-width: 450px) {
+        .red {
+          width: 25%;
+        }
+        .green {
+          width: 75%
+        }
+      }
+
+      @media screen and (min-width: 550px) {
+        .green {
+          width: 50%
+        }
+        .blue {
+          width: 25%
+        }
+      }
+    `}
+    </style>
+  </div>
+)
